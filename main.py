89fc1a56ea9c90744218be3b6618e5c40acd93da
Ole-Magnus Sæther <ole-magnus.saether@kartverket.no>
2025-10-15 13:31:26 +0200

    C3, L1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters, sorted_list
 
 def get_book_text(path):
     with open(path, 'r') as file:
@@ -7,7 +7,23 @@ def get_book_text(path):
 def main():
     book_path = './books/frankenstein.txt'
     book_text = get_book_text(book_path)
-    print(f"Found {count_words(book_text)} total words")
+    words = count_words(book_text)
+    counts = count_characters(book_text)
+    chars_sorted_list = sorted_list(counts)
+    print_report(book_path, words, chars_sorted_list)
+
+
+def print_report(book_path, words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,23 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    text_lower = text.lower()
+    counts = {}
+    for n in text_lower:
+        if n not in counts:
+            counts[n] = 1
+        else:
+            counts[n] += 1
+    return counts
+
+def sorter(items):
+    return items["num"]
+
+def sorted_list(text):
+    sorted_list = []
+    for ch in text:
+        sorted_list.append({"char": ch, "num": text[ch]})
+    sorted_list.sort(reverse=True, key=sorter)
+    return sorted_list
