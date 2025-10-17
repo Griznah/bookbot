009473d78ed57cd85019eac32559186b27dbef34
Ole-Magnus Sæther <ole-magnus.saether@kartverket.no>
2025-10-17 12:56:19 +0200

    Completed version

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/home/linuxbrew/.linuxbrew/bin/python3
+
+import sys
 from stats import count_words, count_characters, sorted_list
 
 def get_book_text(path):
@@ -5,7 +8,10 @@ def get_book_text(path):
         return file.read()
 
 def main():
-    book_path = './books/frankenstein.txt'
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     words = count_words(book_text)
     counts = count_characters(book_text)
